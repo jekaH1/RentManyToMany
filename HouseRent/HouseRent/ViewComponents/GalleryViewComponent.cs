c8b44b202a8dbd9e734625656d8a23c762e67234
JekaH1 <ceyhun100203@gmail.com>
2023-02-28 21:19:55 +0400

    Some bugs fixed

---
+++ HouseRent/HouseRent/ViewComponents/GalleryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using HouseRent.Context;
+using HouseRent.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRent.ViewComponents
+{
+    public class GalleryViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public GalleryViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IViewComponentResult Invoke()
+        {
+            List<GalleryImages> galleryImages = _appDbContext.GalleryImages.Take(6).ToList();
+            return View(galleryImages);
+        }
+    }
+}
